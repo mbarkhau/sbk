7ac30ed16e63e36f94332cb62a1b5cd3ac3edc6c
Manuel Barkhau <mbarkhau@gmail.com>
2019-07-13 13:59:32 +0200

    wip

--- src/sbk/polynom.py
+++ src/sbk/polynom.py
@@ -3,12 +3,16 @@
 #
 # Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
 # SPDX-License-Identifier: MIT
+
 """Polynomial calculation functions.
 
 Mainly lagrange interpolation logic.
 
 Helpful introduction: https://www.youtube.com/watch?v=kkMps3X_tEE
 (Simple introduction to Shamir's Secret Sharing and Lagrange interpolation)
+
+A helpful introduction to Galois Fields:
+https://crypto.stackexchange.com/a/2718
 """
 
 import functools
@@ -18,9 +22,17 @@ import typing as typ
 # The Euclidean GCD algorithm is based on the principle that the
 # greatest common divisor of two numbers does not change if the larger
 # number is replaced by its difference with the smaller number. For
-# example, 21 is the GCD of 252 and 105 (as 252 = 21 × 12 and 105 = 21
-# × 5), and the same number 21 is also the GCD of 105 and 252 − 105 =
-# 147. Since this replacement reduces the larger of the two numbers,
+# example,
+#
+#   GCD(252) == 21  # 252 = 21 × 12
+#   GCD(105) == 21  # 105 = 21 × 5
+#
+#   also
+#
+#   GCD(252 − 105) == 21
+#   GCD(147) == 21
+#
+# Since this replacement reduces the larger of the two numbers,
 # repeating this process gives successively smaller pairs of numbers
 # until the two numbers become equal. When that occurs, they are the
 # GCD of the original two numbers.
@@ -31,94 +43,267 @@ import typing as typ
 # expressed in this way is known as Bézout's identity.
 
 
-class EGCDResult(typ.NamedTuple):
+class XGCDResult(typ.NamedTuple):
     g: int
     s: int  # sometimes called x
     t: int  # sometimes called y
 
 
-def egcd(a: int, b: int) -> EGCDResult:
+XgcdFn = typ.Callable[[int, int], XGCDResult]
+
+
+def check_xgcd(fn: XgcdFn) -> XgcdFn:
+    @functools.wraps(fn)
+    def wrapper(a: int, b: int) -> XGCDResult:
+        res = fn(a, b)
+        assert res.s * a + res.t * b == res.g
+        return res
+
+    return wrapper
+
+
+@check_xgcd
+def xgcd(a: int, b: int) -> XGCDResult:
     """Extended euclidien greatest common denominator."""
     if a == 0:
-        return EGCDResult(b, 0, 1)
+        return XGCDResult(b, 0, 1)
+    else:
+        g, s, t = xgcd(b % a, a)
 
-    q = b // a
-    r = b % a
-    g, s, t = egcd(r, a)
-    t   = t - q * s
-    res = EGCDResult(g, t, s)
-    assert res.s * a + res.t * b == res.g
-    return res
+        q = b // a
+        return XGCDResult(g=g, s=t - q * s, t=s)
 
 
-# def mod_inverse(k, prime):
-#     k = k % prime
-#     if k < 0:
-#         t = egcd(prime, -k).t
-#     else:
-#         t = egcd(prime, k).t
-#     return (prime + t) % prime
+_SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59]
+
+PRIMES = set(_SMALL_PRIMES)
+
+# For this application we want a known prime number as close as
+# possible to our security level; e.g. desired security level of 128
+# bits -- too large and all the ciphertext is large; too small and
+# security is compromised
+
+PRIMES.add(2 ** 521 -   1)  # 13th Mersenne Prime
+PRIMES.add(2 ** 256 - 189)
+PRIMES.add(2 ** 128 - 159)
+PRIMES.add(2 ** 127 -   1)  # 12th Mersenne Prime
+PRIMES.add(2 **  64 -  59)
+PRIMES.add(2 **  32 -   5)
+PRIMES.add(2 **  16 -  15)
+PRIMES.add(2 **   8 -   5)
+
+
+def _is_prime(n: int) -> bool:
+    for p in PRIMES:
+        if n == p:
+            return True
+
+        if n < (p * p) and n % p == 0:
+            return False
+
+    if n > max(_SMALL_PRIMES) ** 2:
+        raise NotImplementedError
+
+    return True
 
 
 FFPolyArithmeticMethod = typ.Callable[['FFPoly', 'FFPoly'], 'FFPoly']
 
 
+def check_ffpoly_compat(a: 'FFPoly', b: 'FFPoly', opname: str) -> None:
+    msg_fmt = "Cannot do '{}' on FFPoly of different {}: {} vs {}."
+
+    if a.p != b.p:
+        raise ValueError(msg_fmt.format(opname, "characteristic", a.p, b.p))
+
+    # if a.n != b.n:
+    #     raise ValueError(msg_fmt.format(opname, "dimension", a.n, b.n))
+
+    len_a = len(a.coeffs)
+    len_b = len(b.coeffs)
+    if len_a != len_b:
+        raise ValueError(msg_fmt.format(opname, "degree", len_a, len_b))
+
+
 def check_arithmetic_args(fn: FFPolyArithmeticMethod) -> FFPolyArithmeticMethod:
+    opname = fn.__name__
+    if opname.startswith("__") and fn.__name__.endswith("__"):
+        opname = opname[2:-2]
+
     @functools.wraps(fn)
     def wrapper(self: 'FFPoly', other: 'FFPoly') -> 'FFPoly':
-        assert self.p == other.p
-        assert len(self.coeffs) == len(other.coeffs)
+        check_ffpoly_compat(self, other, opname)
         return fn(self, other)
 
     return wrapper
 
 
-class FFPoly:
-    """Polynomial in a finite field."""
+Coefficients = typ.Sequence[int]
 
-    coeffs: typ.Tuple[float, ...]
+FFPolyOrNum = typ.Union['FFPoly', int]
+
+
+class FFPoly:
+    """Polynomial/Number in a finite field.
+
+    This can also be thought of as simply as a number in a finite
+    field.
+
+    The coefficients are ordered in ascending powers of x, so
+    FFPoly(2, 5, 3, p=7) is 2x° + 5x¹ + 3x²
+
+    Note that the secret in this case is 2 (the 0th coefficient),
+    which corresponds to the y value when we evaluate at x=0. This is
+    also why other implementations call this value "intercept" or
+    "y_intercept".
+    """
+
+    coeffs: Coefficients
     p     : int
 
-    def __init__(self, *coeffs: float, p: int = 2) -> None:
-        self.coeffs = coeffs
-        self.p      = p
+    def __init__(self, *coeffs: int, p: int) -> None:
+        # NOTE mb: In practice p is always prime, and the operations
+        #   are implemented with the assumtion that it is. If p were
+        #   not prime, then a multiplicative inverse would not exist
+        #   in all cases.
+        assert _is_prime(p)
+        # NOTE mb: I'm not sure the algorithms work for anything
+        #   other than n == 1.
+
+        # aka. characteristic, aka. order
+        self.p = p
+        # Mod p is done so often as a last operation on the
+        # coefficints, that we do it as part of the initialisation.
+        self.coeffs = tuple(c % p for c in coeffs)
+
+    def ffpoly(self, coeffs: Coefficients) -> 'FFPoly':
+        return FFPoly(*coeffs, p=self.p)
 
     @check_arithmetic_args
     def __add__(self, other: 'FFPoly') -> 'FFPoly':
-        coeffs = [(a + b) % self.p for a, b in zip(self.coeffs, other.coeffs)]
-        return FFPoly(*coeffs, p=self.p)
+        coeffs = [a + b for a, b in zip(self.coeffs, other.coeffs)]
+        return self.ffpoly(coeffs)
 
     @check_arithmetic_args
     def __sub__(self, other: 'FFPoly') -> 'FFPoly':
-        coeffs = [(a - b) % self.p for a, b in zip(self.coeffs, other.coeffs)]
-        return FFPoly(*coeffs, p=self.p)
+        coeffs = [a - b for a, b in zip(self.coeffs, other.coeffs)]
+        return self.ffpoly(coeffs)
 
     def __neg__(self) -> 'FFPoly':
-        coeffs = [(-c) % self.p for c in self.coeffs]
-        return FFPoly(*coeffs, p=self.p)
+        return self.ffpoly([-c for c in self.coeffs])
+
+    def __mul__(self, other: FFPolyOrNum) -> 'FFPoly':
+        if isinstance(other, FFPoly):
+            return self._ffpoly_mul(other)
+        elif isinstance(other, int):
+            return self._scalar_mul(other)
+        else:
+            raise NotImplementedError
 
     @check_arithmetic_args
-    def __mul__(self, other: 'FFPoly') -> 'FFPoly':
-        raise NotImplementedError
-        return FFPoly(*[], p=self.p)
+    def _ffpoly_mul(self, other: 'FFPoly') -> 'FFPoly':
+        new_coeffs = [0] * (len(self.coeffs) + 1)
+        for i, a in enumerate(self.coeffs):
+            for j, b in enumerate(other.coeffs):
+                new_coeffs[i + j] += a * b
+                # new_coeffs[i + j] = (a * b) % self.p
 
-    @check_arithmetic_args
+        # poly division
+        return self.ffpoly(new_coeffs)
+
+    def _scalar_mul(self, scalar: int) -> 'FFPoly':
+        return self.ffpoly([c * scalar for c in self.coeffs])
+
     def __div__(self, other: 'FFPoly') -> 'FFPoly':
+        check_ffpoly_compat(self, other, "div")
         raise NotImplementedError
-        return FFPoly(*[], p=self.p)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, FFPoly):
             raise NotImplementedError
 
+        check_ffpoly_compat(self, other, "eq")
         return self.p == other.p and self.coeffs == other.coeffs
 
+    def __call__(self, x: int) -> int:
+        """Evaluate polynomial at x."""
+        terms = [(coeff * x ** exp) for exp, coeff in enumerate(self.coeffs)]
+        return sum(terms) % self.p
+
     def __repr__(self) -> str:
-        coeffs = ", ".join(map(str, self.coeffs))
-        return f"FFPoly({coeffs}, p={self.p})"
+        coef_str = ", ".join(map(str, self.coeffs))
+        return f"FFPoly({coef_str}, p={self.p})"
 
 
-Num = typ.TypeVar('Num', float, FFPoly)
+class GFNum:
+
+    val: int
+    p  : int
+
+    def __init__(self, val: int, p: int) -> None:
+        # NOTE mb: In practice p is always prime, and the operations
+        #   are implemented with the assumtion that it is. If p were
+        #   not prime, then a multiplicative inverse would not exist
+        #   in all cases.
+        assert _is_prime(p)
+        self.val = val
+        self.p   = p
+
+    def gfnum(self, val: int) -> 'GFNum':
+        return GFNum(val % self.p, p=self.p)
+
+    def __add__(self, other: 'GFNum') -> 'GFNum':
+        val = self.val + other.val
+        return self.gfnum(val)
+
+    def __sub__(self, other: 'GFNum') -> 'GFNum':
+        val = self.val - other.val
+        return self.gfnum(val)
+
+    def __neg__(self) -> 'GFNum':
+        return self.gfnum(-self.val)
+
+    def __mul__(self, other: 'GFNum') -> 'GFNum':
+        return self.gfnum(self.val * other.val)
+
+    def _mod_inverse(self) -> 'GFNum':
+        assert self.val >= 0
+        if self.val < 0:
+            t = xgcd(self.p, -self.val).t
+        else:
+            t = xgcd(self.p, self.val).t
+        inv_val = self.p + t
+        return self.gfnum(inv_val)
+
+    def __truediv__(self, other: 'GFNum') -> 'GFNum':
+        return self * other._mod_inverse()
+
+    def __hash__(self) -> int:
+        return hash(self.val) ^ hash(self.p)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, GFNum):
+            raise NotImplementedError
+
+        return self.val == other.val and self.p == other.p
+
+    def __repr__(self) -> str:
+        return f"GFNum({self.val}, p={self.p})"
+
+
+class GF:
+
+    p: int
+
+    def __init__(self, p: int) -> None:
+        assert _is_prime(p)
+        self.p = p
+
+    def __getitem__(self, val: int) -> GFNum:
+        return GFNum(val % self.p, self.p)
+
+
+Num = typ.TypeVar('Num', float, FFPoly, GFNum)
 
 
 class Point(typ.Generic[Num]):
@@ -152,9 +337,8 @@ def prod(vals: typ.Sequence[Num]) -> Num:
     if len(vals) == 0:
         raise ValueError("prod requires at least one value")
 
-    val_iter = iter(vals)
-    accu     = next(val_iter)
-    for val in val_iter:
+    accu = vals[0]
+    for val in vals[1:]:
         accu *= val
     return accu
 
@@ -162,14 +346,23 @@ def prod(vals: typ.Sequence[Num]) -> Num:
 Points = typ.Sequence[Point[Num]]
 
 
-def interpolate(points: Points, at_x: Num) -> Num:
+def _interpolation_terms(points: Points, x: Num) -> typ.Iterable[Num]:
+    for p in points:
+        others = [o for o in points if o != p]
+
+        numer = prod([x   - o.x for o in others])
+        denum = prod([p.x - o.x for o in others])
+
+        yield (p.y * numer) / denum
+
+
+def interpolate(points: Points, x: Num) -> Num:
     r"""Interpolate y value at x for a polynomial.
 
     # \delta_i(x) = \prod{ \frac{x - j}{i - j} }
     # \space
     # \text{for} \space j \in C, j \not= i
     """
-
     if len(points) < 2:
         raise ValueError("Cannot interpolate with fewer than two points")
 
@@ -177,20 +370,8 @@ def interpolate(points: Points, at_x: Num) -> Num:
     if len(x_vals) != len(set(x_vals)):
         raise ValueError("Points must be distinct {points}")
 
-    def sum_parts() -> typ.Iterable[Num]:
-        for p in points:
-            other_points = list(points)
-            other_points.remove(p)
-
-            cur_x = p.x
-            numer = prod([at_x  - o.x for o in other_points])
-            denum = prod([cur_x - o.x for o in other_points])
-
-            yield (p.y * numer) / denum
-
-    parts = iter(sum_parts())
-    rv    = next(parts)
-
-    for p in parts:
-        rv += p
-    return rv
+    # Start with addititve identity (aka. Zero, aka. 0.0).
+    # Initialization is done with x - x (vs. just using literal 0.0),
+    # so that the implementation also works for FFPoly.
+    zero = x - x
+    return sum(_interpolation_terms(points, x), zero)
--- test/test_polynom.py
+++ test/test_polynom.py
@@ -1,7 +1,9 @@
+import random
+
 from sbk.polynom import *
 
 
-# Case 3: egcd(240, 46)
+# Case 3: xgcd(240, 46)
 # | i |     qi−1     |         ri        |        si       |          ti         |
 # |---|--------------|-------------------|-----------------|---------------------|
 # | 0 |              | 240               | 1               | 0                   |
@@ -13,10 +15,10 @@ from sbk.polynom import *
 # | 6 | 4 ÷ 2 = 2    | 4 − 2 × 2 = 0     | 5 − 2 × −9 = 23 | −26 − 2 × 47 = −120 |
 
 
-def test_egcd():
-    assert egcd(252, 105) == EGCDResult(g=21, s=-2, t=5)
-    assert egcd(105, 252) == EGCDResult(g=21, s=5, t=-2)
-    assert egcd(240,  46) == EGCDResult(g=2 , s=-9, t=47)
+def test_xgcd():
+    assert xgcd(252, 105) == XGCDResult(g=21, s=-2, t=5)
+    assert xgcd(105, 252) == XGCDResult(g=21, s=5, t=-2)
+    assert xgcd(240,  46) == XGCDResult(g=2 , s=-9, t=47)
 
 
 def test_prod():
@@ -25,25 +27,83 @@ def test_prod():
     assert prod([2, 3, 4]) == 24
 
 
+def test_ffpoly_init():
+    x = FFPoly(2, 5, p=7)
+
+    assert x.coeffs == (2, 5)
+
+    assert x.p == 7
+
+
+def alt_eval(coeffs, p):
+    """An alternative implementation of eval for validation.
+
+    https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing
+    """
+
+    def eval_fn(x):
+        accu = 0
+        for coeff in reversed(coeffs):
+            accu *= x
+            accu += coeff
+            accu %= p
+        return accu
+
+    return eval_fn
+
+
+def test_ffpoly_eval():
+    a = FFPoly(2, 3, 4, p=7)
+    e = alt_eval([2, 3, 4], p=7)
+
+    # 2*0° + 3*0¹ + 4*0² == (2 + 3 + 4) % 7 == 9 % 7 == 2
+    assert a(x=0) == (2 + 3 + 4) % 7
+    assert a(x=0) == e(x=0)
+
+    # 2*1° + 3*1¹ + 4*1² == (2 + 3 + 4) % 7 == 9 % 7 == 2
+    assert a(x=1) == (2 + 3 + 4) % 7
+    assert a(x=1) == e(x=1)
+
+    # 2*2° + 3*2¹ + 4*2² == (2 + 6 + 16) % 7 == 24 % 7 == 3
+    assert a(x=2) == (2 + 6 + 16) % 7
+    assert a(x=2) == e(x=2)
+
+    # 2*3° + 3*3¹ + 4*3² == (2 + 9 + 36) % 7 == 47 % 7 == 5
+    assert a(x=3) == (2 + 9 + 36) % 7
+    assert a(x=3) == e(x=3)
+
+
+def test_ffpoly_eval_fuzz():
+    primes = sorted(PRIMES)
+    for _ in range(100):
+        p      = random.choice(primes)
+        coeffs = [random.randint(0, p - 1) for _ in range(random.randint(1, 9))]
+        a      = FFPoly(*coeffs, p=p)
+        e      = alt_eval(coeffs, p=p)
+        for _ in range(10):
+            x = random.randint(0, p - 1)
+            assert a(x) == e(x)
+
+
 def test_ffpoly_add_sub():
-    a = FFPoly(2.0, 2.0, p=3)
-    b = FFPoly(1.0, 2.0, p=3)
+    a = FFPoly(2, 2, p=3)
+    b = FFPoly(1, 2, p=3)
     assert (a + b) - b == a
     assert -a + b == b - a
 
 
 def test_ffpoly_mul():
-    a = FFPoly(2.0, 2.0, p=3)
-    b = FFPoly(1.0, 2.0, p=3)
+    a = FFPoly(2, 2, p=7)
+    b = FFPoly(1, 2, p=7)
+    c = FFPoly(2, 2 + 4, 4, p=7)
 
-    assert a * b ==
+    assert a * b == c
 
+    # a = FFPoly(2, 2, p=3)
+    # b = FFPoly(1, 2, p=3)
+    # c = FFPoly(1, 1, p=3)
 
-def test_ffpoly_div():
-    a = FFPoly(2.0, 2.0, p=3)
-    b = FFPoly(1.0, 2.0, p=3)
-    c = FFPoly(1.0, 1.0, p=3)
-    assert a / b == c
+    # assert a * b == c
 
 
 def test_interpolate_deg1_float():
@@ -52,7 +112,7 @@ def test_interpolate_deg1_float():
     interp_y = interpolate(points[:-1], at_x)
     assert interp_y == at_y
 
-    assert interpolate(points[:-1], at_x=0.5) == 1.00
+    assert interpolate(points[:-1], x=0.5) == 1.00
 
 
 def test_interpolate_deg2_float():
@@ -66,12 +126,41 @@ def test_interpolate_deg2_float():
     interp_y = interpolate(points[:-1], at_x)
     assert interp_y == at_y
 
-    assert interpolate(points[:-1], at_x=0.5) == 0.25
+    assert interpolate(points[:-1], x=0.5) == 0.25
 
 
-def test_interpolate_deg1_ffpoly():
-    assert False
+def test_gf_arithmetic():
+    gf = GF(7)
+
+    zero = gf[0]
+    one = gf[1]
+    two = gf[2]
+    five = gf[5]
+
+    assert gf[0] == gf[7]
+    assert gf[1] == gf[8]
+
+    assert one - one == zero
+    assert two - two == zero
+
+    assert one * two == two
+    assert two * two + one == five
+    assert two * two * two == gf[1]
+
+    assert two / one == two
+    assert two / two == one
 
 
-def test_interpolate_deg2_ffpoly():
-    assert False
+def test_interpolate_gf():
+    gf = GF(7)
+    points = [
+        Point(gf[0], gf[0]),
+        Point(gf[1], gf[1]),
+        Point(gf[2], gf[4]),
+        Point(gf[3], gf[9]),
+    ]
+    at_x, at_y = points[-1]
+    interp_y = interpolate(points[:-1], at_x)
+    assert interp_y == at_y
+
+    assert interpolate(points[:-1], x=gf[1] / gf[2]) == gf[1] / gf[4]

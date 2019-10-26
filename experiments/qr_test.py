a63c1b30f134987bc06eda1a6467318208f94563
Manuel Barkhau <mbarkhau@gmail.com>
2019-10-26 19:58:27 +0000

    add experiments/qr_test.py

--- experiments/qr_test.py
+++ experiments/qr_test.py
@@ -1,23 +1,35 @@
 import io
+import sys
+
 import qrcode
 
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Hello World!')
+def main(args=sys.argv[1:]):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    if args:
+        data = " ".join(args)
+    else:
+        data = "https://gitlab.com/mbarkhau/sbk#air-gap-computer"
 
-bw_buf = io.StringIO()
-wb_buf = io.StringIO()
+    qr.add_data(data)
 
-qr.print_ascii(out=bw_buf, invert=False)
-qr.print_ascii(out=wb_buf, invert=True)
+    bw_buf = io.StringIO()
+    wb_buf = io.StringIO()
 
-bw_lines = bw_buf.getvalue().splitlines()
-wb_lines = wb_buf.getvalue().splitlines()
+    qr.print_ascii(out=bw_buf, invert=False)
+    qr.print_ascii(out=wb_buf, invert=True)
 
-for bw_line, wb_line in zip(bw_lines, wb_lines):
-    print(bw_line, wb_line)
+    bw_lines = bw_buf.getvalue().splitlines()
+    wb_lines = wb_buf.getvalue().splitlines()
+
+    print()
+    for bw_line, wb_line in zip(bw_lines, wb_lines):
+        print(bw_line, wb_line)
+
+if __name__ == '__main__':
+    main()
